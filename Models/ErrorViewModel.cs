a8871be16751dda100393a99ed3651bc53138cdf
AyaNabil90 <144095807+AyaNabil90@users.noreply.github.com>
2025-12-03 11:42:55 +0200

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _360.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
